c0a4141cd14bf04bed82d151373f120df3703970
Fan Li <30782622+fanlidublin@users.noreply.github.com>
2017-10-12 16:35:20 +0100

    fan

--- Code_Buffer/Code_Package/k_means_for_iris.py
+++ Code_Buffer/Code_Package/k_means_for_iris.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
 from sklearn.decomposition import PCA
+from datetime import datetime
 
 # import some data to play with
 iris = datasets.load_iris()
