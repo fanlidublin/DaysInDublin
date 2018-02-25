351b6cd4834cb98d3132c8e05e7a71fd6a9e82bb
Fan Li <li.fan1@ucdconnect.ie>
2018-02-25 13:06:20 +0000

    fan

---
+++ Data Structure in Java/src/fanli/topic3/SLinkedList.java
@@ -0,0 +1,14 @@
+package fanli.topic3;
+
+/** Singly linked list . */
+public class SLinkedList {
+	protected Node head; // head node of the list
+	protected long size; // number of nodes in the list
+
+	/** Default constructor that creates an empty list */
+	public SLinkedList() {
+		head = null;
+		size = 0;
+	}
+	// ... update and search methods would go here ...
+}
